e12f3847bd2471a4077f75e3b30a96f41aa4f6aa
timlunar <lunar.tim@gmail.com>
2020-12-16 19:22:51 +0100

    Added routing for logedin users and basic users

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "../views/Home";
+import Login from "../views/Login";
+import Dashboard from "@/views/Dashboard";
 
 Vue.use(VueRouter);
 
@@ -11,17 +13,37 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/login",
+    name: "Login",
+    component: Login
+  },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: Dashboard,
+    meta: { requiresAuth: true }
   }
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  },
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem('user')
+
+  if(to.matched.some(record => record.meta.requiresAuth)) {
+    if(!loggedIn) {
+      next('/')
+    }else
+      next()
+  }else
+    next()
+})
 
 export default router;
