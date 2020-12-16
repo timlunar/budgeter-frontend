ad0fd608cb54e593305d623642b3c0da148344ad
timlunar <lunar.tim@gmail.com>
2020-12-16 19:21:33 +0100

    Loader component imported

---
+++ src/components/gadgets/Loader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex flex-wrap align-items-center justify-content-center p-0 bt-loader">
+    <div class="lds-ripple">
+      <div v-for="i in 5" :key="i"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Loader"
+}
+</script>
+
+<style scoped>
+
+</style>
