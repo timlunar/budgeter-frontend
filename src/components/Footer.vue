7f642f684b615ea1fae788ea6d1f2a89bc3e964b
timlunar <lunar.tim@gmail.com>
2020-12-16 19:22:17 +0100

    Navbar and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="d-flex flex-wrap justify-content-center col-12 p-0 pt-5" id="footer">
+    <div class="d-flex flex-column align-items-center col-xl-3 col-md-4 col-12">
+      <h6 class="green-text pt-xl-0 pt-5">ABOUT</h6>
+      <span>Budgeter Inc.</span>
+      <span>budgeter@gmail.com</span>
+      <span>Ljubljana, Slovenia</span>
+    </div>
+    <div class="d-flex flex-column align-items-center col-xl-3 col-md-4 col-12 pt-xl-0 pt-3">
+      <h6 class="green-text pt-xl-0 pt-5">RESOURCES</h6>
+      <span>Budgeter</span>
+      <span>Budgeter</span>
+    </div>
+    <div class="d-flex flex-column align-items-center col-xl-3 col-md-4 col-12 pt-xl-0 pt-3 pb-5">
+      <h6 class="green-text pt-xl-0 pt-5">FOLLOW US</h6>
+      <div class="d-flex flex-wrap">
+        <font-awesome-icon class="footer-icons" :icon="['fab', 'twitter']" />
+        <font-awesome-icon class="footer-icons" :icon="['fab', 'facebook']" />
+        <font-awesome-icon class="footer-icons" :icon="['fab', 'instagram']" />
+      </div>
+    </div>
+
+    <span class="text-center col-12 p-0 pt-5 pb-3 footer-licence" style="color: lightgray"><b class="green-text">Budgeter </b>Copyright © 2020</span>
+  </footer>
+</template>
+
+<script>
+export default {
+name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="align-items-center fixed-top">
+    <b-navbar class="navbar-inside pt-3" toggleable="lg">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <router-link to="/" v-scroll-to="'#home'" class="nav-link">Home</router-link>
+          <b-nav-item href="#"
+                      v-scroll-to="'#guidelines'">Guidelines</b-nav-item>
+          <b-nav-item href="#"
+                      v-scroll-to="'#functionalities'">Functionalities</b-nav-item>
+          <b-nav-item href="#"
+                      v-scroll-to="'#promotion'">Feedbacks</b-nav-item>
+          <b-nav-item href="#"
+                      v-scroll-to="'#packages'">Packages</b-nav-item>
+          <b-nav-item href="#"
+                      v-scroll-to="'#footer'">About us</b-nav-item>
+
+          <router-link to="/login" class="btn btn-outline-success btn-navbar nav-link pt-2" v-if="$store.state.user == null">Login</router-link>
+          <button class="btn btn-outline-success btn-navbar nav-link" v-else @click="logout">Logout</button>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </header>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+name: "Navbar",
+  methods: {
+    logout() {
+      this.$store.dispatch('logout');
+      return axios.post('https://arcane-castle-36997.herokuapp.com/api/logout').then();
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
